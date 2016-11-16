72b539cf2748a012213fd6be9d524a838aa5d206
Alexandra <Alexandraeya@gmail.com>
2016-11-16 10:11:55 -0800

    im trying to fix the colors

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,22 +2,26 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 
+var color = "#ffcccc"
+var color2 = "#ffcc66"
+
+
 function circle1() {
   var stepone = document.createElementNS(namespace, "circle")
   stepone.setAttribute("cx",230 )
   stepone.setAttribute("cy",90 )
   stepone.setAttribute("r", 20)
-  stepone.setAttribute("fill", "#ffcccc")
+  stepone.setAttribute("fill",)
   var canvas = document.getElementById("game-board")
   canvas.appendChild(stepone)
 }
-}
+
 function circle2() {
   var stepthree = document.createElementNS(namespace, "circle")
   stepthree.setAttribute("cx",90 )
   stepthree.setAttribute("cy",90)
   stepthree.setAttribute("r", 20)
-  stepthree.setAttribute("fill", "#ffcccc")
+  stepthree.setAttribute("fill",)
   var canvas = document.getElementById("game-board")
   canvas.appendChild(stepthree)
 }
@@ -26,7 +30,7 @@ function circle3() {
   stepfour.setAttribute("cx",160)
   stepfour.setAttribute("cy",160)
   stepfour.setAttribute("r", 20)
-  stepfour.setAttribute("fill", "#ffcccc")
+  stepfour.setAttribute("fill",)
   var canvas = document.getElementById("game-board")
   canvas.appendChild(stepfour)
 }
@@ -35,7 +39,7 @@ function circle4() {
   stepfive.setAttribute("cx",230)
   stepfive.setAttribute("cy",230)
   stepfive.setAttribute("r", 20)
-  stepfive.setAttribute("fill", "#ffcccc")
+  stepfive.setAttribute("fill",)
   var canvas = document.getElementById("game-board")
   canvas.appendChild(stepfive)
 }
@@ -44,18 +48,43 @@ function circle5() {
   stepsix.setAttribute("cx",90)
   stepsix.setAttribute("cy",230)
   stepsix.setAttribute("r", 20)
-  stepsix.setAttribute("fill", "#ffcccc")
+  stepsix.setAttribute("fill",)
   var canvas = document.getElementById("game-board")
   canvas.appendChild(stepsix)
 }
-
-function square1() {
-  var squareone = document.createElementNS(namespace,"rect")
-  squareone.setAttribute("width",10)
-  squareone.setAttribute("height",10)
-  squareone.setAttribute("x",130)
-  squareone.setAttribute("y",60)
-  squareone.setAttribute("fill","orange")
+function circle6() {
+  var stepseven = document.createElementNS(namespace, "circle")
+  stepseven.setAttribute("cx",160)
+  stepseven.setAttribute("cy",90)
+  stepseven.setAttribute("r",20)
+  stepseven.setAttribute("fill",)
   var canvas = document.getElementById("game-board")
-  canvas.appendChild(squareone)
+  canvas.appendChild(stepseven)
+}
+function circle7() {
+  var step8 = document.createElementNS(namespace, "circle")
+  step8.setAttribute("cx",90)
+  step8.setAttribute("cy",160)
+  step8.setAttribute("r",20)
+  step8.setAttribute("fill",)
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(step8)
+}
+function circle8() {
+  var step9 = document.createElementNS(namespace, "circle")
+  step9.setAttribute("cx",230)
+  step9.setAttribute("cy",160)
+  step9.setAttribute("r",20)
+  step9.setAttribute("fill",)
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(step9)
+}
+function circle9() {
+  var step10 = document.createElementNS(namespace, "circle")
+  step10.setAttribute("cx",160)
+  step10.setAttribute("cy",230)
+  step10.setAttribute("r",20)
+  step10.setAttribute("fill",)
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(step10)
 }
